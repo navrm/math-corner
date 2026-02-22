02e63ec12335a4fa742e0b5ff311ceb0346dc2a8
navrm <etwinning71@gmail.com>
2026-02-22 18:25:26 +0200

    Add files via upload

---
+++ srcApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from './components/Banner';
+
+function App() {
+  return (
+    <div className="app">
+      <Banner />
+      <h1>Καλώς ήρθατε στην Μαθηματική Γωνιά μας!</h1>
+      {/* Μπορείς να προσθέσεις περισσότερα components εδώ */}
+    </div>
+  );
+}
+
+export default App;
---
+++ srccomponentsBanner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Banner() {
+  return (
+    <div className="banner">
+      <h2>Η Μαθηματική Γωνιά μας</h2>
+      <p>Εκεί όπου η λογική συναντά τη δημιουργία</p>
+    </div>
+  );
+}
+
+export default Banner;
