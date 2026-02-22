9409851a50ca15aacd8b2d77c2f43536508ef16d
navrm <etwinning71@gmail.com>
2026-02-22 19:30:45 +0200

    Create App.jsx file for main app component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>Η Μαθηματική Γωνιά μας</h1>
+      </header>
+      <main>
+        <p>Welcome to our math corner!</p>
+        <p>Here, we explore fascinating math concepts and solve interesting problems.</p>
+        <p>Stay tuned for more awesome math content!</p>
+      </main>
+    </div>
+  );
+};
+
+export default App;
